f517146e69cc3d022f500298ed5eaa41b4e37b68
Kirill Biktimirov <kirillbiktya@users.noreply.github.com>
2021-10-14 13:58:22 +0300

    Так как вынес библиотеку для работы с вк в отдельный проект, убрал все отсюда, здесь будет только бот

--- toxic_state_bot/__init__.py
+++ toxic_state_bot/__init__.py
@@ -1,5 +1,5 @@
 import flask
-from toxic_state_bot.vk import types
+# from toxic_state_bot.vk import types
 
 app = flask.Flask(__name__)
 
@@ -7,6 +7,6 @@ app = flask.Flask(__name__)
 @app.route('/', methods=['POST'])
 def index():
     # b4 need to authorize request!
-    request_object = types.VKUpdate.de_json(flask.request.json)
+    # request_object = types.VKUpdate.de_json(flask.request.json)
     print(request_object.u_object.liker_id)
     return 'ok', 200
